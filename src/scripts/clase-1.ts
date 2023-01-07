44d5a1ba29273d5de5a4512fa2133af3acba6ba2
Javier Marin <nano.world.contact@gmail.com>
2023-01-07 17:13:01 -0600

    clase 1
    Signed-off-by: Javier Marin <nano.world.contact@gmail.com>

---
+++ src/scripts/clase-1.ts
@@ -0,0 +1,29 @@
+type Conf = {
+  el: HTMLVideoElement | null
+}
+
+export class MediaPlayer {
+  media: HTMLVideoElement | null;
+
+  constructor(config: Conf) {
+    this.media = config.el
+  }
+
+  play() {
+    this.media?.play()
+  }
+
+  pause() {
+    this.media?.pause();
+  }
+
+  togglePlay() {
+    this.media?.paused ? this.media.play() : this.media?.pause();
+  }
+}
+
+const video = document.querySelector('video');
+const player = new MediaPlayer({ el: video });
+
+const button = document.querySelector('button');
+button?.addEventListener('click', () => player.togglePlay());
