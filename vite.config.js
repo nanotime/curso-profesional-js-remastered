bce1f2717bba0bc91f48361dcf5c3173574474c7
Javier Marin <nano.world.contact@gmail.com>
2023-01-07 17:12:41 -0600

    Cambios de configuración
    Signed-off-by: Javier Marin <nano.world.contact@gmail.com>

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'src/pages/*.html'),
+      },
+    },
+  },
+})
